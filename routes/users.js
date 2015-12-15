1c9912995515ead3a96bd08fdd091440600ce794
Mithilesh Said <s.mit7816@gmail.com>
2015-12-15 13:01:37 +0530

    Testing user hotels

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,19 @@ var router = express.Router();
 var User = require('../models/User');
 
 /* GET users listing. */
+router.get('/hotels', function (req, res, next) {
+  if (!req.session.user) {
+    return res.json({ result: false, message: 'You need to login before you can view your saved hotels', data: null });
+  }
+  User.findOne({ _id: req.session.user._id }, function (error, user) {
+    if (error) {
+      res.status(500);
+      return res.json({ result: false, message: 'Internal server error', data: null });
+    }
+    return res.json({ result: true, message: 'User hotels found', data: user.hotels });
+  });
+});
+
 router.get('/', function(req, res, next) {
   if (!req.session.user)
     return res.json({ result: false, message: 'Unauthorised access. You need to login!', data: null });
@@ -46,7 +59,7 @@ router.post('/login', function (req, res, next) {
       }
       if (!user)
         return res.json({ result: false, message: 'No user found with that email id', data: null });
-      req.session.user = { email: user.email };
+      req.session.user = { email: user.email, _id: user._id };
       return res.json({ result: false, message: 'Login successfull', data: null });
     });
 });
@@ -55,6 +68,27 @@ router.get('/logout', function (req, res, next) {
   if (req.session.user)
     delete req.session.user;
   return res.json({ result: false, message: 'Logged out successfully', data: null });
+});
+
+router.post('/save_hotel', function (req, res, next) {
+  if (!req.session.user) {
+    return res.json({ result: false, message: 'You need to login before you can save a hotel entry', data: null });
+  }
+  var hotel = req.body;
+  User.findOne({ _id: req.session.user._id }, function (error, user) {
+    if (error) {
+      res.status(500);
+      return res.json({ result: false, message: 'Internal server error', data: null });
+    }
+    user.hotels.push(hotel);
+    user.save(function (err, savedUser) {
+      if (err) {
+        res.status(500);
+        return res.json({ result: false, message: 'Internal server error', data: null });
+      }
+      return res.json({ result: true, message: 'User saved successfully', data: savedUser.hotels });
+    });
+  });
 });
 
 module.exports = router;
